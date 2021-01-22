788863188749c516c79ee37a10709b431ec46c04
dom111 <dom111@users.noreply.github.com>
2021-01-22 20:42:32 +0000

    Use published package version for `core-wonder`

--- HangingGardens.d.ts
+++ HangingGardens.d.ts
@@ -1,4 +1,3 @@
 import Wonder from '@civ-clone/core-wonder/Wonder';
-export declare class HangingGardens extends Wonder {
-}
+export declare class HangingGardens extends Wonder {}
 export default HangingGardens;
